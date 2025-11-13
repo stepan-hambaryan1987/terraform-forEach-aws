c3c1c810be75c802008cbcddc712c5d6aa19be98
stepan hambaryan <stepan-hambaryan1987@mail.ru>
2025-11-13 21:13:16 +0400

    module aws to create VPC Network block

---
+++ terraform-aws-forEach/main.tf
@@ -0,0 +1,293 @@
+# -------- aws vpc block --------#
+
+# vpc
+# Internet Gateway 
+# security_group (open ingress icmp, 22, 80, 443 ------ egress 0.0.0.0/0)
+# subnets (two public and two private)
+# route table (one public subnets and one private subnets)
+# route_table_association (two)
+# one eip
+# one NAT Gateway
+
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_ciders
+
+  tags = {
+    Name = "${var.env}-vpc"
+  }
+}
+
+
+# --- Internet Gateway ---
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+
+resource "aws_security_group" "web_sg" {
+  name        = "${var.env}-web-sg"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress = concat(
+    [
+      for port in var.allowed_ports : {
+        description      = "Allow inbound port ${port}"
+        from_port        = port
+        to_port          = port
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = []
+        prefix_list_ids  = []
+        security_groups  = []
+        self             = false
+      }
+    ],
+    [
+      {
+        description      = "Allow ICMP (ping)"
+        from_port        = -1
+        to_port          = -1
+        protocol         = "icmp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = []
+        prefix_list_ids  = []
+        security_groups  = []
+        self             = false
+      }
+    ]
+  )
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-SG"
+  }
+}
+
+
+
+resource "aws_subnet" "public-subnet" {
+  for_each                = var.public_subnets_ciders
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value
+  map_public_ip_on_launch = true
+  availability_zone       = element(data.aws_availability_zones.available.names, index(keys(var.public_subnets_ciders), each.key))
+
+
+  tags = {
+    Name = "${var.env}-${each.key}"
+    AZ   = element(data.aws_availability_zones.available.names, index(keys(var.public_subnets_ciders), each.key))
+
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  for_each                = var.private_subnets_ciders
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value
+  map_public_ip_on_launch = false
+  availability_zone       = element(data.aws_availability_zones.available.names, index(keys(var.private_subnets_ciders), each.key))
+
+
+  tags = {
+    Name = "${var.env}-${each.key}"
+    AZ   = element(data.aws_availability_zones.available.names, index(keys(var.private_subnets_ciders), each.key))
+
+  }
+}
+
+# --- Public Route Table ---
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.env}-public-rt"
+  }
+}
+
+# --- Associate Public Subnets ---
+resource "aws_route_table_association" "public_assoc" {
+  for_each = aws_subnet.public-subnet
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+# --- NAT Gateways ---
+# Create one NAT gateway per public subnet (HA)
+resource "aws_eip" "nat" {
+  #for_each = aws_subnet.public-subnet
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.env}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-subnet["pub-1"].id
+  depends_on    = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "${var.env}-nat"
+  }
+}
+
+# --- Private Route Table ---
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-private-rt"
+  }
+}
+
+# --- Route private subnets to NAT (choose first NAT for simplicity) ---
+resource "aws_route" "private_default" {
+  route_table_id         = aws_route_table.private-rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+# --- Associate Private Subnets ---
+resource "aws_route_table_association" "private_assoc" {
+  for_each = aws_subnet.private-subnet
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private-rt.id
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# resource "aws_security_group" "web_sg" {
+#   name        = "web-security-group"
+#   description = "Allow inbound traffic"
+#   vpc_id      = aws_vpc.main.id
+
+#   ingress = [
+#     for port in var.allowed_ports : {
+#       description = "Allow inbound port ${port}"
+#       from_port   = port
+#       to_port     = port
+#       protocol    = "tcp"
+#       cidr_blocks = ["0.0.0.0/0"]
+#     }
+#   ]
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "${var.env}-SG"
+#   }
+# }
+
+
+
+
+
+
+# resource "aws_security_group" "web_sg" {
+#   name        = "web-security-group"
+#   description = "Allow inbound"
+#   vpc_id      = aws_vpc.main.id
+
+#   tags = {
+#     Name = "web-sg"
+#   }
+# }
+
+# resource "aws_security_group_rule" "ingress_rules" {
+#   for_each = toset(var.allowed_ports)
+
+#   type              = "ingress"
+#   from_port         = each.value
+#   to_port           = each.value
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws_security_group.web_sg.id
+# }
+
---
+++ terraform-aws-forEach/variable.tf
@@ -0,0 +1,34 @@
+variable "env" {
+  description = "for resourses names"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_ciders" {
+  description = "for vpc ciders"
+  type        = string
+  default     = "10.10.0.0/16"
+}
+
+variable "allowed_ports" {
+  type    = list(number)
+  default = [22, 80, 443]
+}
+
+variable "public_subnets_ciders" {
+  description = "ciders for public subnets"
+  type        = map(string)
+  default = {
+    pub-1 = "10.10.1.0/24"
+    pub-2 = "10.10.2.0/24"
+  }
+}
+
+variable "private_subnets_ciders" {
+  description = "ciders for private subnets"
+  type        = map(string)
+  default = {
+    priv-1 = "10.10.3.0/24"
+    priv-2 = "10.10.4.0/24"
+  }
+}
